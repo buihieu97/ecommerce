072754c6f91fb9345005cc8a14901d218b3f4281
buihieu97 <buivanhieu19972@gmail.com>
2022-12-12 09:48:42 +0700

    add action scroll

---
+++ app.js
@@ -0,0 +1,44 @@
+const productContainer = document.querySelector(".product-list .box-card");
+const boxDots = Array.from(document.querySelectorAll(".box-dot"));
+
+boxDots.forEach((e, index) => {
+  const sliderContainer = e.parentElement;
+  sliderContainer.style.scrollBehavior = "smooth";
+  const sliderItems = Array.from(sliderContainer.children).filter(
+    (e, index) => index < sliderContainer.children.length - 1
+  );
+  const widthItem = sliderItems[0].clientWidth;
+  const widthItemSlider = widthItem * sliderItems.length;
+  const containerWidth = sliderContainer.clientWidth;
+  const countItemVisible = (containerWidth / widthItem).toFixed();
+  const countDot = widthItemSlider / countItemVisible / widthItem;
+  new Array(countDot).fill(0).forEach((e, i) => {
+    let dot = document.createElement("div");
+    dot.classList = "dot";
+    if (i === 0) {
+      dot.classList = "dot active";
+    }
+    boxDots[index].appendChild(dot);
+  });
+  const dots = [...e.children];
+
+  dots.forEach((e, i) => {
+    e.addEventListener("click", (e) => {
+      sliderContainer.scrollLeft = widthItem * countItemVisible * i;
+      if (i === dots.length - 1) {
+        sliderContainer.scrollLeft = widthItem * 2 * countItemVisible * i;
+      }
+      //   console.log(sliderContainer.children[0].clientWidth);
+    });
+  });
+  sliderContainer.addEventListener("scroll", (e) => {
+    let indexDotActive = Math.floor(
+      e.target.scrollLeft /
+        (sliderContainer.children[0].clientWidth * countItemVisible)
+    );
+    dots.forEach((e) => {
+      e.classList.remove("active");
+    });
+    dots[indexDotActive].classList = "dot active";
+  });
+});
